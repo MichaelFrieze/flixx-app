ad1131f403fb4cef8128a95cd9afbbda4d7f084c
Michael Frieze <michaelfrieze@icloud.com>
2023-04-16 16:19:49 -0400

    movie details page

--- js/script.js
+++ js/script.js
@@ -80,6 +80,87 @@ async function displayPopularShows() {
   });
 }
 
+// Display Movie Details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  console.log(movie.homepage);
+
+  const div = document.createElement('div');
+
+  div.innerHTML = `
+      <div class="details-top">
+      <div>
+        ${
+          movie.poster_path
+            ? `
+          <img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+          />  
+          `
+            : `
+          <img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+          />  
+          `
+        }
+      </div>
+      <div>
+        <h2>${movie.title}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${movie.vote_average.toFixed(1)} / 10
+        </p>
+        <p class="text-muted">Release Date: ${movie.release_date}</p>
+        <p>
+          ${movie.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+          ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+        </ul>
+        ${
+          movie.homepage &&
+          `<a
+              href="${movie.homepage}"
+              target="_blank"
+              class="btn"
+            >
+              Visit Movie Homepage
+            </a>`
+        }
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Movie Info</h2>
+      <ul>
+        <li><span class="text-secondary">Budget:</span>
+          ${movie.budget ? `$${movie.budget.toLocaleString('en-US')}` : `N/A`}
+        </li >
+        <li><span class="text-secondary">Revenue:</span> ${
+          movie.revenue ? `$${movie.revenue.toLocaleString('en-US')}` : `N/A`
+        }</li>
+        <li><span class="text-secondary">Runtime:</span> ${toHoursAndMinutes(
+          movie.runtime
+        )}</li>
+        <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">${movie.production_companies
+        .map((company) => `<span>${company.name}</span>`)
+        .join(', ')}
+      </div>
+    </div>
+  `;
+
+  document.querySelector('#movie-details').appendChild(div);
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
   // Register your key at https://www.themoviedb.org/settings/api and enter here
@@ -119,6 +200,17 @@ function highlightActiveLink() {
   });
 }
 
+function toHoursAndMinutes(totalMinutes) {
+  const hours = Math.floor(totalMinutes / 60);
+  const minutes = totalMinutes % 60;
+
+  return hours >= 1
+    ? hours >= 2
+      ? `${hours} hours ${minutes > 0 ? `and ${minutes} minutes` : ''}`
+      : `${hours} hour ${minutes > 0 ? `and ${minutes} minutes` : ''}`
+    : `${minutes} minutes`;
+}
+
 // Init app
 function init() {
   switch (global.currentPage) {
@@ -130,7 +222,7 @@ function init() {
       displayPopularShows();
       break;
     case '/movie-details.html':
-      console.log('Movie Details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('TV Details');
